313b694c4787808c472124688e1518b8fa52874b
yapsharlene <sayap2@up.edu.ph>
2021-04-28 14:46:14 +0800

    Added doors

---
+++ CS191_Group3/Assets/Door.cs
@@ -0,0 +1,25 @@
+﻿//PROGRAMMER: Sharlene Yap
+//This script is used to toggle the opening/closing of doors.
+
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Animator anim;
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = door.GetComponent<Animator>();
+    }
+    
+    void OnTriggerStay(Collider c)
+    {
+        if (c.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("Interact");
+        }
+    }
+ 
+}
